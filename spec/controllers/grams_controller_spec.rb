b5ac950a7f0e58ccd7ae3e595bf035432bf42a0c
adriana <adrianamolello@gmail.com>
2016-04-04 16:11:42 +0000

    Added edit, show & update actions for grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,7 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-	describe "grams#index action" do 
+	describe "grams#update" do
+    it "should allow users to successfully update grams" do
+      p = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: p.id, gram: { message: 'Changed'}
+      expect(response).to redirect_to root_path
+      p.reload
+      expect(p.message).to eq "Changed"
+    end
+
+    it "should have a 404 error if the gram cannont be found" do
+      patch :update, id: "YOLOSWAG", gram: {message: 'Changed'}
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with a http status of unprocessable_entity" do
+      p = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: p.id, gram: { message: ''}
+      expect(response).to have_http_status(:unprocessable_entity)
+      p.reload
+      expect(p.message).to eq "Initial Value"
+    end
+  end
+
+
+  describe "grams#edit" do
+    it "should successfully show the edit form if the gram id found" do
+      p = FactoryGirl.create(:gram)
+      get :edit, id: p.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error message if the gram is not found" do
+      get :edit, id: 'TACOCAT'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+
+  describe "grams#show action" do
+    it "should successfully show the page if the gram if found" do
+      gram = FactoryGirl.create(:gram)
+      get :show, id: gram.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+      get :show, id: 'TACOCAT'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "grams#index action" do 
 		it "should successfully show the page" do
 			get :index
 			expect(response).to have_http_status(:success)
