4931796962f311bf2445730cfac0296960357aec
adriana <adrianamolello@gmail.com>
2016-04-11 21:54:24 +0000

    fixed syntax in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,9 +10,7 @@ class CommentsController < ApplicationController
   
   private
 
-  def render_not_found(status=:not_found)
-    render text: "#{status.to_s.titleize} :(", status: status
-  end
+ 
 
   
   def comment_params
